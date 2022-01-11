7362f9614b7a65a6930648f92b70fba14a159087
Aayus Jain <aayushjain6020@gmail.com>
2022-01-11 14:48:31 +0530

    initial commit +proj

---
+++ src/screens/HomeScreen.js
@@ -0,0 +1,45 @@
+import React from 'react'
+import { FlatList, StyleSheet, Text, View } from 'react-native'
+
+const HomeScreen = () => {
+    const Friends=[
+        {"name":"John","age":30},
+        {"name":"Peter","age":25},
+        {"name":"Amy","age":35},
+        {"name":"Hannah","age":45},
+        {"name":"Michael","age":85},
+        {"name":"Sandy","age":15},
+        {"name":"Betty","age":105},
+        {"name":"Richard","age":55},
+        {"name":"Susan","age":65},
+        {"name":"Vicky","age":75},
+    ];
+    return (
+        <FlatList 
+            data={Friends}
+            keyExtractor={(item)=>item.name}
+            renderItem={({item})=>{
+                return(
+                    <View style={styles.listItem}>
+                        <Text>Name: {item.name}</Text>
+                        <Text>Age: {item.age}</Text>
+                    </View>
+                )
+            }}
+        />
+    )
+}
+
+export default HomeScreen
+
+const styles = StyleSheet.create({
+    listItem:{
+        backgroundColor:'#ccc',
+        marginVertical:10,
+        padding:10,
+        
+
+    },
+})
+
+
