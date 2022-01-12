087d6daaaab6f146dc16901f0c94087281c19853
Aayus Jain <aayushjain6020@gmail.com>
2022-01-12 13:00:43 +0530

    Stack Navigator IMPLEMENTED

--- App.js
+++ App.js
@@ -1,49 +1,42 @@
-import React from 'react';
-import { Platform, StyleSheet, Text, TouchableHighlight, TouchableOpacity, TouchableNativeFeedback, TouchableWithoutFeedback, View } from 'react-native';
-import HomeScreen from './src/screens/HomeScreen';
-import ListScreen from './src/screens/ListScreen';
+import * as React from 'react';
+import { Button, View, Text } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import HomeScreen from './src/screens/HomeScreen';
+import ListScreen from './src/screens/ListScreen'
+import List from './src/screens/List';
 
 
-const Tab = createBottomTabNavigator();
+function DetailsScreen({ navigation }) {
+  return (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Text>Details Screen</Text>
+      <Button
+        title="Go to Details... again"
+        onPress={() => navigation.push('Details')}
+      />
+      <Button title="Go to Home" onPress={() => navigation.navigate('Home')} />
+      <Button title="Go back" onPress={() => navigation.goBack()} />
+      <Button
+        title="Go back to first screen in stack"
+        onPress={() => navigation.popToTop()}
+      />
+    </View>
+  );
+}
 
+const Stack = createNativeStackNavigator();
 
-const App = () => {
-  
-
+function App() {
   return (
     <NavigationContainer>
-             <Tab.Navigator>
-                <Tab.Screen name="Home" component={HomeScreen}
-                
-                  options={
-                    
-                    {headerShown: false,
-                      tabBarIcon: ({ focused, color, size }) => {
-                        return <Text style={{ fontSize: size, color: color }}>🏠</Text>
-                      }
-                    }
-                  }
-                />
-                <Tab.Screen name="list" component={ListScreen}
-                  options={
-                    {headerShown: false,
-                      tabBarIcon: ({ focused, color, size }) => {
-                        return <Text style={{ fontSize: size, color: "purple" }}>⚙️</Text>
-                      }
-                    }
-                  }
-                />
-
-        </Tab.Navigator>
-  </NavigationContainer>
-  )
-};
-
-const styles = StyleSheet.create({
- 
-});
+      <Stack.Navigator initialRouteName="Home">
+        <Stack.Screen name="Home" component={HomeScreen} />
+        <Stack.Screen name="List" component={ListScreen} />
+        <Stack.Screen name="Items" component={List} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
 
 export default App;
-
--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,43 +1,45 @@
 import React from 'react'
-import { FlatList, StyleSheet, Text, View } from 'react-native'
+import { Button, FlatList, StyleSheet, Text, View } from 'react-native'
 
-const HomeScreen = () => {
-    const Friends=[
-        {"name":"John","age":30},
-        {"name":"Peter","age":25},
-        {"name":"Amy","age":35},
-        {"name":"Hannah","age":45},
-        {"name":"Michael","age":85},
-        {"name":"Sandy","age":15},
-        {"name":"Betty","age":105},
-        {"name":"Richard","age":55},
-        {"name":"Susan","age":65},
-        {"name":"Vicky","age":75},
-    ];
+
+
+
+
+const HomeScreen = ({ navigation }) => {
+
     return (
-        <FlatList 
-            data={Friends}
-            keyExtractor={(item)=>item.name}
-            renderItem={({item})=>{
-                return(
-                    <View style={styles.listItem}>
-                        <Text>Name: {item.name}</Text>
-                        <Text>Age: {item.age}</Text>
-                    </View>
-                )
-            }}
+        <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+        <Text>Home Screen</Text>
+        <Button
+          title="Go to Lists"
+          style={{marginVertical:10}}
+          onPress={() => {
+            navigation.navigate('List', {
+              itemId: 86,
+              otherParam: 'anything you want here',
+            });
+          }}
         />
+        <Button
+          title="Go to Details"
+          style={{marginVertical:10}}
+          onPress={() => {
+            navigation.navigate('Items', {
+              itemId: 86,
+              otherParam: 'anything you want here',
+            });
+          }}
+        />
+      </View>
+        
     )
 }
+
 
 export default HomeScreen
 
 const styles = StyleSheet.create({
-    listItem:{
-        backgroundColor:'#ccc',
-        marginVertical:10,
-        padding:10,
-    },
+
 })
 
 
